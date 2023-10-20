52b4944a4989b9c0c470077c481b0dc0caed8788
Kgarmon99 <119528390+Kgarmon99@users.noreply.github.com>
2023-10-20 19:38:53 +0000

    Added emojis where needed :)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def beautiful_progress_bar(value, goal, length=30):
     fill_length = int(length * (progress / goal))
     gap_length = length - fill_length
     bar = "█" * fill_length + " " * gap_length
-    return f"Progress: [{bar}] ${gap:.2f} away from your next milestone"
+    return f"Progress: [{bar}] ${gap:.2f} away from your next milestone🏁"
 
 # Define your financial milestones (in dollars)
 milestones = [1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12]  # $100 to 1 trillion dollars
@@ -15,16 +15,16 @@ print("Hello, I'm MoneyBot, your financial motivator.")
 print("Let's start your day with financial goals in mind.")
 
 # How will you make more money?
-make_more_money = input("\nHow will you make more money today? ")
+make_more_money = input("\nHow will you MAKE more money today?🤑 ")
 
 # How will you save more money?
-save_more_money = input("How will you save more money today? ")
+save_more_money = input("How will you SAVE more money today?📈 ")
 
 # Cash on Hand
-cash_on_hand = float(input("How much cash do you have on hand? $"))
+cash_on_hand = float(input("How much cash do you have on hand? 💵$"))
 
 # Current Net Worth (You can input your actual net worth)
-current_net_worth = float(input("\nYour current net worth (assets - debts): $"))
+current_net_worth = float(input("\nYour current net worth (assets - debts): 🥇$"))
 
 # Calculate the next upcoming milestone
 next_milestone = next((milestone for milestone in milestones if milestone > current_net_worth), None)
@@ -41,7 +41,7 @@ if next_milestone is not None:
 else:
     print("\nCongratulations! You've reached your 1 Trillion dollar goal. Keep it up!")
 
-print("\nThank you for starting your day with a financial vision.")
+print("\nThank you for starting your day with a financial vision.🐐")
 
 # Money-Making Update
 print(f"\nToday's Money-Making Plan: {make_more_money}")
@@ -63,4 +63,4 @@ cash_on_hand += daily_income - daily_expenses
 
 print(f"\nCash on Hand: ${cash_on_hand:.2f}")
 
-print("\nMake it a great day!")
+print("\nMake it a great day!🕺🏽")
