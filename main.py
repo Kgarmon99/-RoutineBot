5de41a8595d4a9b5b5faad7062c697a204c27ad2
Kgarmon99 <119528390+Kgarmon99@users.noreply.github.com>
2023-10-25 14:28:23 +0000

    Added Morning + Nightly routine. Added "View Goals" feature

--- main.py
+++ main.py
@@ -1,71 +1,109 @@
-# Daily Money Adventure with Milestones and Gamification 🚀
-
 def beautiful_progress_bar(value, goal, length=30):
-    progress = min(value, goal)
-    gap = goal - progress
-    fill_length = int(length * (progress / goal))
-    gap_length = length - fill_length
-    bar = "█" * fill_length + " " * gap_length
-    return f"Progress: [{bar}] ${gap:.2f} away from your next milestone 🏁"
+  progress = min(value, goal)
+  gap = goal - progress
+  fill_length = int(length * (progress / goal))
+  gap_length = length - fill_length
+  bar = "█" * fill_length + " " * gap_length
+  return f"Progress: [{bar}] ${gap:.2f} away from your next milestone 🏁"
 
-# Define your financial milestones (in dollars)
-milestones = [1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12]  # $100 to 1 trillion dollars
-Name = input("Good morning, I'm MoneyBot!! your finace co-pilot. What is your name? ")
-print(f"Welcome {Name}! to the MoneyVerse! 🚀")
-print("Let's begin your journey of riches & success. 💼💰")
+def get_float_input(prompt):
+  while True:
+      try:
+          return float(input(prompt))
+      except ValueError:
+          print("Please enter a valid numerical value.")
 
-# How will you make more money today?
-make_more_money = input("\nYour mission: How will you INCREASE your income today? 🤑 ")
+def calculate_remaining_to_next_milestone(current_net_worth, milestones):
+  next_milestone = next((milestone for milestone in milestones if milestone > current_net_worth), None)
+  if next_milestone is not None:
+      return next_milestone - current_net_worth
+  else:
+      return 0.0
 
-# How will you save more money today?
-save_more_money = input("Your strategy: What's your PLAN for SAVING money today? 📈 ")
+def print_milestone_map(milestones, current_net_worth):
+  print("\n🗺️ Financial Milestone Map 🗺️")
+  next_milestone = next((milestone for milestone in milestones if milestone > current_net_worth), None)
+  for i, milestone in enumerate(milestones, 1):
+      progress_marker = "🚀" if milestone == next_milestone else "  "
+      print(f"{progress_marker} Level {i}: ${milestone:.2f}")
 
-# Cash on Hand
-cash_on_hand = float(input("Your resources: How much cash do you have on hand? 💵 $"))
+def set_and_review_morning_routine():
+  make_more_money = input("\nYour morning mission: What is one thing you can do today to INCREASE your income? 🤑 ")
+  save_more_money = input("Your morning strategy: What's one thing you can do today to cut costs and SAVE money? 💰 ")
 
-# Your Current Net Worth (Assets - Debts)
-current_net_worth = float(input("\nYour status: What's your current net worth? 🥇 $"))
+  with open("morning_intentions.txt", "w") as file:
+      file.write(f"Morning Mission: {make_more_money}\nMorning Strategy: {save_more_money}")
 
-# Calculate the next upcoming milestone
-next_milestone = next((milestone for milestone in milestones if milestone > current_net_worth), None)
+  print("Morning intentions saved. Start your day with purpose!")
 
-# Financial Milestone Map
-print("\n🗺️ Financial Milestone Map 🗺️")
-for i, milestone in enumerate(milestones, 1):
-    progress_marker = "🚀" if milestone == next_milestone else "  "
-    print(f"{progress_marker} Level {i}: ${milestone:.2f}")
+def set_and_review_nightly_routine():
+  daily_income = get_float_input("\n💰 Nightly reflection: How much gold did you find today? 🤑 $")
+  daily_expenses = get_float_input("💸 Nightly reflection: How much treasure did you spend today? 💎 $")
 
-# Progress Bar Visualization
-if next_milestone is not None:
-    print("\n🌟 Your Current Progress 🌟")
-    print(beautiful_progress_bar(current_net_worth, next_milestone))
-    print(f"🚀 Your next milestone: Level {milestones.index(next_milestone) + 1} 🚀")
-else:
-    print("\n🏆 Congratulations! You've reached your ultimate financial goal. Keep it up! 🏆")
+  with open("morning_intentions.txt", "r") as file:
+      intentions = file.read()
 
-print("\nThank you for joining the Money Adventure! 🐐")
+  print("Morning Intentions:")
+  print(intentions)
 
-# Today's Money-Making Quest
-print(f"\n📜 Today's Money-Making Quest 📜")
-print(f"Objective: {make_more_money} 📈")
+  print(f"\n💰 Your Wealth Status: You now have ${daily_income - daily_expenses:.2f} in your treasure chest. 💰")
+  print("Nightly reflections completed. Prepare for a restful night!")
 
-# Today's Money-Saving Challenge
-print(f"\n💼 Today's Money-Saving Challenge 💼")
-print(f"Challenge: {save_more_money} 📈")
+def check_daily_goals(daily_income, daily_expenses, make_more_money, save_more_money):
+  print("\n🎯 Daily Goals Summary 🎯")
+  print(f"Morning Mission: {make_more_money}")
+  print(f"Morning Strategy: {save_more_money}")
+  print(f"💰 Earned: ${daily_income:.2f} | 💸 Spent: ${daily_expenses:.2f}")
 
-# Progress to Next Milestone
-if next_milestone is not None:
-    remaining_to_next_milestone = next_milestone - current_net_worth
-    print(f"\n🎯 Your Next Milestone Quest 🎯")
-    print(f"Quest: Earn ${remaining_to_next_milestone:.2f} to reach the next milestone.")
-else:
-    print("\n🏆 Congratulations, you've reached your ultimate financial goal! 🏆")
+  if daily_income >= float(make_more_money) and daily_expenses <= float(save_more_money):
+      print("Congratulations! You met your daily goals. Keep it up!")
+  else:
+      print("You didn't meet your daily goals. Don't worry, there's always tomorrow!")
 
-# Gamified Income and Expenses
-daily_income = float(input("\n💰 Income Update: How much gold did you find today? 🤑 $"))
-daily_expenses = float(input("💸 Expenses Update: How much treasure did you spend today? 💎 $"))
-cash_on_hand += daily_income - daily_expenses
+def display_milestones(milestones, current_net_worth):
+  print_milestone_map(milestones, current_net_worth)
 
-print(f"\n💰 Your Wealth Status: You now have ${cash_on_hand:.2f} in your treasure chest. 💰")
+def main():
+  Name = input("Hello, I'm MoneyBot, your finance co-pilot. What's your name? ")
+  print(f"Welcome to the MoneyVerse, {Name}! 🚀")
+  print("Let's get this money! 💰")
 
-print("\n🚀 Continue your Money Adventure! 🚀")
+  routine_choice = input("\nDo you want to set morning intentions, do nightly reflections, or show goals? (morning/night/goals): ").lower()
+
+  if routine_choice == "morning":
+      set_and_review_morning_routine()
+  elif routine_choice == "night":
+      set_and_review_nightly_routine()
+      # Continue with gamified features, personal goals, and milestones here...
+      # Define your financial milestones (in dollars)
+      milestones = [1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13]  # Added an extra milestone for multi-trillionaire goal
+
+      # Collect daily income and expenses from the user
+      daily_income = get_float_input("💰 How much did you earn today? 🤑 $")
+      daily_expenses = get_float_input("💸 How much did you spend today? 💎 $")
+
+      make_more_money, save_more_money = 0.0, 0.0  # Initialize for the nightly routine
+
+      # Update current net worth based on daily income and expenses
+      current_net_worth = daily_income - daily_expenses
+
+      # Calculate the next upcoming milestone
+      next_milestone = next((milestone for milestone in milestones if milestone > current_net_worth), None)
+
+      print_milestone_map(milestones, current_net_worth)  # Moved milestone display here
+
+      remaining_to_next_milestone = calculate_remaining_to_next_milestone(current_net_worth, milestones)
+
+      if next_milestone is not None:
+          print("\n🌟 Your Current Progress 🟢")
+          print(beautiful_progress_bar(current_net_worth, current_net_worth + remaining_to_next_milestone))
+          print(f"🚀 Your next milestone: Level {milestones.index(current_net_worth + remaining_to_next_milestone) + 1} 🚀")
+      else:
+          print("\n🏆 Congratulations! You've reached your ultimate financial goal. Keep it up! 🏆")
+  elif routine_choice == "goals":
+      # Display financial milestones at the beginning
+      milestones = [1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13]  # Added an extra milestone for multi-trillionaire goal
+      display_milestones(milestones, 0)  # Start with a net worth of 0
+
+if __name__ == "__main__":
+  main()
